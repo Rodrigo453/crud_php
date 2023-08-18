2e408f78db2cfeebbc1c8c48d08c11dac2d6a09e
Rodrigo453 <79484389+Rodrigo453@users.noreply.github.com>
2023-08-18 15:14:34 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
 $servidor = "10.217.0.65:3306";
 $usuario = "root";
-$senha = "";
-$dbname = "test";
+$senha = "password";
+$dbname = "banco_php";
 
 //Criar a conexao
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
