d098d9b771c82a444ce6a79c8c165a78496f73a6
Rodrigo453 <79484389+Rodrigo453@users.noreply.github.com>
2023-08-20 18:41:15 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,18 @@
 <?php
-$servidor = "mysql:host=localhost;port=3306";
-$usuario = "root";
-$senha = "root";
-$dbname = "banco_php";
-
+$servidor = getenv("MYSQL_SERVICE_HOST");
+$porta = getenv("MYSQL_SERVICE_PORT");
+$usuario = getenv("databaseuser");
+$senha = getenv("databasepassword");
+$dbname = getenv("database");
 //Criar a conexao
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+if(!$conn)
+{
+  echo("Não conectado");
+}
+else
+{
+  echo("Conectado);
+}
+?>
